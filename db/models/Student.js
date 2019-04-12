4717474fa71aadc4e70d5f112d164e672af3c430
Dave <dave.e.siegel@gmail.com>
2019-04-12 01:36:04 -0400

    models broken out into own files

---
+++ db/models/Student.js
@@ -0,0 +1,41 @@
+const { db } = require("../db");
+const Sequelize = db.Sequelize;
+
+const Student = db.define("student", {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
+  },
+  gpa: {
+    type: Sequelize.DECIMAL(10, 1),
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
+  }
+});
+
+module.exports = Student;
---
+++ db/models/index.js
@@ -0,0 +1,7 @@
+const Student = require("./Student");
+const Campus = require("./Campus");
+
+Campus.hasMany(Student);
+Student.belongsTo(Campus);
+
+module.exports = { Campus, Student };
