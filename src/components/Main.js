4d03d6551b064b5d51f2f2ec50c98eac8beaac19
Dave <dave.e.siegel@gmail.com>
2019-04-12 15:56:16 -0400

    main component updates store from db

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { refreshCampuses, refreshStudents } from "../store";
+
+const mapDispatchToProps = dispatch => ({
+  refreshCampuses: () => dispatch(refreshCampuses()),
+  refreshStudents: () => dispatch(refreshStudents())
+});
+
+class Main extends Component {
+    componentDidMount(){
+        this.props.refreshCampuses().catch(e => console.log(e.message));
+        this.props.refreshStudents().catch(e => console.log(e.message));
+    }
+    render(){
+        return (
+            <h1>HELLO THERE!!</h1>
+        );
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Main);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { HashRouter, Route } from "react-router-dom"
+import Main from "./components/Main";
+import { Provider } from "react-redux";
+import store from "./store";
+ 
 const root = document.querySelector('#root');
-ReactDOM.render(<hr />, root);
-
-
+ReactDOM.render(<Provider store={store}><HashRouter><Route component={Main} /></HashRouter></Provider>, root);
