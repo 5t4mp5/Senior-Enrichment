2df29576be7aca5bd1efd24d700a5758613f4349
Dave <dave.e.siegel@gmail.com>
2019-04-13 16:58:00 -0400

    Feature: Remove Campus and Student

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { removeCampus } from "../store";
 
-const Campus = ({ campus }) => (
+const mapDispatchToProps = dispatch => ({
+  deleteCampus: campus => dispatch(removeCampus(campus))
+});
+
+const Campus = ({ campus, deleteCampus }) => (
   <li className="list-group-item">
     <span>
       <Link to={`/campuses/${campus.id}`}>{campus.name}</Link><img src={campus.imgUrl} />
     </span>
+    <button className="btn btn-danger" type="button" onClick={() => deleteCampus(campus)}>X</button>
   </li>
 );
 
-export default Campus;
+export default connect(null, mapDispatchToProps)(Campus);
