0de0e3ba8ea378e97081b993e7b31391685fcf60
Dave <dave.e.siegel@gmail.com>
2019-04-13 10:30:47 -0400

    get student and get campus

--- server/app/router.js
+++ server/app/router.js
@@ -3,15 +3,26 @@ const router = express.Router();
 const { Student, Campus } = require("../db");
 
 router.get("/campuses", (req, res, next) => {
-    Campus.findAll()
-      .then(campuses => res.json(campuses))
-      .catch(e => console.log(e.message));
-  });
-  
-  router.get("/students", (req, res, next) => {
-    Student.findAll()
-      .then(students => res.json(students))
-      .catch(e => console.log(e.message));
-  });
+  Campus.findAll()
+    .then(campuses => res.json(campuses))
+    .catch(e => console.log(e.message));
+});
 
-  module.exports = router;
+router.get("/campuses/:id", (req, res, next) => {
+  Campus.findByPk(req.params.id, { include: [Student] })
+    .then(campus => res.json(campus))
+    .catch(e => console.log(e.message));
+});
+
+router.get("/students", (req, res, next) => {
+  Student.findAll()
+    .then(students => res.json(students))
+    .catch(e => console.log(e.message));
+});
+
+router.get("/students/:id", (req, res, next) => {
+  Student.findByPk(req.params.id, { include: Campus })
+    .then(student => res.json(student));
+});
+
+module.exports = router;
